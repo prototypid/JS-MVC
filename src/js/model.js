0001211ed815a40f1cadd94c3afb315b9d27525c
darq <prototypid@gmail.com>
2023-08-18 17:37:38 +0530

    Add method to return 10 results

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from "./config.js";
+import { API_URL, RESULT_PER_PAGE } from "./config.js";
 import { getJSON } from "./helpers.js";
 
 export const state = {
@@ -6,6 +6,8 @@ export const state = {
   search: {
     query: "",
     results: [],
+    resultsPerPage: RESULT_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -48,4 +50,13 @@ export const loadSearchResults = async function (query) {
     console.error(err);
     throw err;
   }
+};
+
+export const getSearchResultspage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
